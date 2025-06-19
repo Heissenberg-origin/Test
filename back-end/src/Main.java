b8a50ded4f08cbbd887ff0465bcb28000e907bb5
Heissenberg-origin <3021205404@gmail.com>
2025-06-19 14:52:18 +0800

    测试提交

--- back-end/src/Main.java
+++ back-end/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        test();
+    }
+
+    public static void test()
+    {
+        System.out.println("This is a tets");
     }
 }
